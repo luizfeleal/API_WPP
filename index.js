a4b4bcbaae0decb4e54ea7f8e4d577d8e0616f9b
Luiz Felipe <felipelearaujo@gmail.com>
2022-02-13 16:19:51 -0300

    atualizando diretorio de imagem

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function start (client) {
   });
   socket.on('ready', () => {
       setTimeout(function (){
-          socket.emit('ready', './out.png'); //https://www.andersonbrandao.com.br/images/out.png
+          socket.emit('ready', 'https://apiwpp.herokuapp.com/images/out.png'); //https://www.andersonbrandao.com.br/images/out.png
       }, 3000)
   });
 })
