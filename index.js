1531a6f9e29c8bbe421dc9f01f8b2d3ad09c96f0
Luiz Felipe Leal de Araujo <felipelearaujo@gmail.com>
2022-03-15 17:51:23 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const suportMessage = require('./messages.json')
 });*/
 
 
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 
 app.get('/home', (req, res)=> {
-    res.render('home');
+    res.render(__dirname + '/views/index.html'); //'home'
 })
 
 //app.unsubscribe(express.static(__dirname + '/images'));
