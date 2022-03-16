9f637c109e332720d7b98b36c0b20b4127bbebb5
Luiz Felipe <felipelearaujo@gmail.com>
2022-03-16 10:45:50 -0300

    .

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const suportMessage = require('./messages.json')
 });*/
 
 
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/public/views'));
 
 app.get('/home', (req, res)=> {
     //res.send('home'); //'home'
