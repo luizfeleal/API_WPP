ba78e8ccc742b5354100c090c0913ea9a02c9bfa
Luiz Felipe <felipelearaujo@gmail.com>
2022-03-15 21:44:22 -0300

    .

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.get('/home', (req, res)=> {
     //res.send('home'); //'home'
-    res.render('/views/grupowpp.html')
+    res.render('/views/grupowpp')
 })
 
 //app.unsubscribe(express.static(__dirname + '/images'));
