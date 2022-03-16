9a2d5b1281b295a114968076d9a92ead4fb84408
Luiz Felipe <felipelearaujo@gmail.com>
2022-03-16 11:13:53 -0300

    .

--- index.js
+++ index.js
@@ -60,12 +60,12 @@ io.on('connection', (socket)=>{
     
     var imageBuffer = response;
     
-    axios.post("https://www.andersonbrandao.com.br/criaImagem.php", {code: imageBuffer['data'].toString('base64')}) .then(function(resposta){
+    /*axios.post("https://www.andersonbrandao.com.br/criaImagem.php", {code: imageBuffer['data'].toString('base64')}) .then(function(resposta){
       console.log(resposta.data);
-    })
+    })*/
 
-    console.log(imageBuffer['data'].toString('base64'));
-
+    //console.log(imageBuffer['data'].toString('base64'));
+    
     require('fs').writeFile(
       './public/images/out.png', //./images/out.png
       imageBuffer['data'],
